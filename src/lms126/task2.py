c543b404775c537fd3122cf21987977872bbb274
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-01 23:30:19 +0100

    Solution task 2

--- src/lms126/task2.py
+++ src/lms126/task2.py
@@ -1,13 +1,9 @@
 from to_do import TODO
+
+
 def task2():
-    return
-"""
-INPUT height of rectangle
-INPUT base of rectangle 
-Calculate area of rectangle
-OUTPUT area of rectangle
-
-
-
-
-    """
+    return """
+    INPUT height of rectangle
+    INPUT base of rectangle
+    Calculate area of rectangle
+    OUTPUT area of rectangle """
