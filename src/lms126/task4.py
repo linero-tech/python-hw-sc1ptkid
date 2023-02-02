e4d8eabb919d1583726d49207f863124618fdba0
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-02 17:29:42 +0100

    Solution task 4

--- src/lms126/task4.py
+++ src/lms126/task4.py
@@ -2,4 +2,8 @@ from to_do import TODO
 
 
 def task4():
-    return TODO("Erase this line and write your solution")
+    return """
+    INPUT the value of X
+    INPUT the value of Y
+    Calculate the greater value of X and Y
+    OUTPUT the greater value """
