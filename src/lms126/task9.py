817a717ff58edfc1682eefcbc72a5b996287af43
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-04 12:02:31 +0100

    Solution task 9

--- src/lms126/task9.py
+++ src/lms126/task9.py
@@ -2,4 +2,17 @@ from to_do import TODO
 
 
 def task9():
-    return TODO("Erase this line and write your solution")
+    return """
+    START    
+    INPUT read user input from card
+    COMPUTE Determine password match
+    IF match
+    OUTPUT balance on card
+    INPUT user input withdraw amount  
+    COMPUTE determine balance
+    IF match
+    SET (New balance)
+    INIT withdraw process
+    ENDIF
+    
+    """
