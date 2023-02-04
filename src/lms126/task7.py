a47e383d6d5f82c0f5080453bcf24f31e5c44d49
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-04 08:18:55 +0100

    Update task7.py

--- src/lms126/task7.py
+++ src/lms126/task7.py
@@ -4,7 +4,7 @@ from to_do import TODO
 def task7():
     return """
     START
-    FOR Numbers in range (2, 1S02, 2)
+    FOR Numbers in range (2, 102, 2)
     OUTPUT print even Numbers from 1 to 100
     END FOR
     """
