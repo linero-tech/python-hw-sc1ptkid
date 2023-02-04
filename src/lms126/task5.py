0c47ed5fda305572e4336c55f30c1798363c9d4c
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-04 21:17:17 +0100

    Update task5.py

--- src/lms126/task5.py
+++ src/lms126/task5.py
@@ -5,11 +5,10 @@ def task5():
     return """
     
     START
-    GET X == 1 to 8:
+    INPUT GET X == 1 to 8:
     IF X == 1 to 8:
-    ELIF == print ("Monday to Sunday")
-    ELSE Do nothing
-    OUTPUT print the ("Day of the week")
-    END IF
+    OUTPUT == print ("Monday to Sunday")
+    ELSE Do nothing  
+    ENDIF
                
     """
