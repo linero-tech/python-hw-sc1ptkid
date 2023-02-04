0153be8c9403ce1f78a43996734b53223e0e17c5
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-04 07:59:06 +0100

    Solution task 6

--- src/lms126/task6.py
+++ src/lms126/task6.py
@@ -2,4 +2,9 @@ from to_do import TODO
 
 
 def task6():
-    return TODO("Erase this line and write your solution")
+    return """
+    START
+    FOR Numbers in range (5, 101, 5)
+    OUTPUT print Numbers multiples of 5
+    END FOR
+    """
