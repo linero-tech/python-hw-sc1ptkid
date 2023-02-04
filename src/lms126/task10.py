84e2b2c1098610bee469f2dc99d23fd081ebb385
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-04 17:17:14 +0100

    Solution Task 10

--- src/lms126/task10.py
+++ src/lms126/task10.py
@@ -2,4 +2,14 @@ from to_do import TODO
 
 
 def task10():
-    return TODO("Erase this line and write your solution")
+    return """ 
+    START
+    Display print ("Enter user email")
+        INPUT user email
+        SET email to user
+    Display print ("Enter a user password up to six characters")
+        INPUT user password
+        WHILE len password < 6 character        
+        SET password to user
+    Display print ("Account created")
+    END """
