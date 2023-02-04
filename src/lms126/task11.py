538bcf46e0876b65e65d0c77f7f0c8b967469fe7
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-04 18:12:56 +0100

    Solution task 11

--- src/lms126/task11.py
+++ src/lms126/task11.py
@@ -2,4 +2,21 @@ from to_do import TODO
 
 
 def task11():
-    return TODO("Erase this line and write your solution")
+    return """
+# Convert Celsius to Fahrenheit
+
+START
+INPUT celsius value
+SET fahrenheit_value = (Celsius_value * 9/5) + 32
+DISPLAY print Fahrenheit_value
+END
+
+# Convert Fahrenheit to Celsius
+
+START
+INPUT Fahrenheit value
+SET Celsius_value = (Fahrenheit_value - 32) * 5/9
+DISPLAY print Celsius_value
+END 
+
+"""
