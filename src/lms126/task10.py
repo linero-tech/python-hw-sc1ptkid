430a545aa379f30021698ef3983c1a27a5e5af74
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-05 09:06:34 +0100

    Update task10.py

--- src/lms126/task10.py
+++ src/lms126/task10.py
@@ -4,12 +4,12 @@ from to_do import TODO
 def task10():
     return """ 
     START
-    Display print ("Enter user email")
+    OUTPUT print ("Enter user email")
         INPUT user email
         SET email to user
-    Display print ("Enter a user password up to six characters")
+    OUTPUT print ("Enter a user password up to six characters")
         INPUT user password
         WHILE len password < 6 character        
         SET password to user
-    Display print ("Account created")
+    OUTPUT print ("Account created")
     END """
