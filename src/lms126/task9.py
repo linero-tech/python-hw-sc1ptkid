fd7631534696e58615d441921af4690591f8de2a
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-05 14:05:42 +0100

    Update task9.py

--- src/lms126/task9.py
+++ src/lms126/task9.py
@@ -5,14 +5,14 @@ def task9():
     return """
     START    
     INPUT read user input from card
-    COMPUTE Determine password match
-    IF match
-    OUTPUT balance on card
+        COMPUTE Determine password match
+        IF match
+        OUTPUT print balance on card
     INPUT user input withdraw amount  
-    COMPUTE determine balance
-    IF match
-    SET (New balance)
-    INIT withdraw process
+        COMPUTE determine balance
+        IF match
+        Calculate add (New balance)
+        THEN withdraw money
     ENDIF
     
     """
