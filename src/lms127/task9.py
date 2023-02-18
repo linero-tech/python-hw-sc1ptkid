64e571be94f874105c0852c1dc7b3affba0c01b9
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-18 22:59:25 +0100

    Update task9.py

--- src/lms127/task9.py
+++ src/lms127/task9.py
@@ -2,6 +2,9 @@ from to_do import TODO
 
 
 def task9(sentence, character):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = sentence.lower().count(character)
+    return bool(result)
+
+
+if __name__ == "__main__":
+    print(task9(sentence="I code In KOTLIN", character="i"))
