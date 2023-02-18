6c0699f0069bd1bba83bf7963f320530f78c7748
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-18 21:01:49 +0100

    Update task5.py

--- src/lms127/task5.py
+++ src/lms127/task5.py
@@ -4,7 +4,7 @@ from to_do import TODO
 def task5(value_for_a, value_for_b):
     a = value_for_a
     b = value_for_b
-    b, a = a, b #swaping value_for_a for value_for_b
+    b, a = a, b
 
     # Do not erase or change the below statement
     return a, b
