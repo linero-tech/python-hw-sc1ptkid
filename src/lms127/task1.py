643f5c75322200a5593989195f00b6942f0e581d
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-18 13:59:42 +0100

    Update task1.py

--- src/lms127/task1.py
+++ src/lms127/task1.py
@@ -2,10 +2,10 @@ from to_do import TODO
 
 
 def task1():
-    country = "panama"
-    capital = "panama city"
-    currency = "usd"
-    country_name = f"Country:{country.title()}\nCapital:{capital.title()}\nCurrency:{currency.upper()}"
+    country = "sweden"
+    capital = "stockholm"
+    currency = "sek"
+    country_name = f"Country: {country.title()}\nCapital: {capital.title()}\nCurrency: {currency.upper()}"
     return country_name
 
 
