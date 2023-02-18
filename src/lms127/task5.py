75f115e0bc2a8ba13cb1363eea33c9f5d7babfb3
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-18 20:53:19 +0100

    Update task5.py

--- src/lms127/task5.py
+++ src/lms127/task5.py
@@ -4,8 +4,11 @@ from to_do import TODO
 def task5(value_for_a, value_for_b):
     a = value_for_a
     b = value_for_b
-
-    TODO("Your solutions goes in here.")
+    b, a = a, b
 
     # Do not erase or change the below statement
     return a, b
+
+
+if __name__ == "__main__":
+    print (task5(1, 2))
