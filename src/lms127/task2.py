631d57650a82b7570f9b8a234274a32fbd40e9b5
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-18 16:26:19 +0100

    Update task2.py

--- src/lms127/task2.py
+++ src/lms127/task2.py
@@ -4,8 +4,8 @@ from to_do import TODO
 def task2():
     name = "Camilo"
     country = "Sweden"
-    month = "January"
-    print(f'"Hello {name}, you are from {country} and you were born in {month}"')
+    month_of_birth = "January"
+    print(f'"Hello {name}, you are from {country} and you were born in {month_of_birth}"')
 
 
 
