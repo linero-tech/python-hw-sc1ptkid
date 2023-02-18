2c2b347a93418902181e28be36eea0057aeb9aa5
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-18 16:22:02 +0100

    Update task2.py

--- src/lms127/task2.py
+++ src/lms127/task2.py
@@ -5,7 +5,7 @@ def task2():
     name = "Camilo"
     country = "Sweden"
     month = "January"
-    print(f'"Hello! My name is {name} I was born in {country} on a cold {month} night 1788"')
+    print(f'"Hello {name}, you are from {country} and you were born in {month}"')
 
 
 
