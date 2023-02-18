69e2d6b0d65efdba3db4d03102816630f456130c
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-18 21:26:42 +0100

    Update task6.py

--- src/lms127/task6.py
+++ src/lms127/task6.py
@@ -2,6 +2,9 @@ from to_do import TODO
 
 
 def task6(base1, base2, height):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = ((base1 + base2) / 2) * height
+    return result
+
+
+if __name__ == "__main__":
+    print(task6(10.0, 20.0, 1.0))
