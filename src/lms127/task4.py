8a213a0192005f848bd5591cca2d240868a60429
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-20 19:30:08 +0100

    Update task4.py

--- src/lms127/task4.py
+++ src/lms127/task4.py
@@ -5,7 +5,8 @@ def task4(base, height):
     result = 0.5 * base * height
     return result
 
+
 if __name__ == "__main__":
-   print (task4(base=5.0, height=10.0))
+   print(task4(base=5.0, height=10.0))
    print(task4(base=0.0, height=10.0))
    print(task4(base=50.0, height=0.0))
