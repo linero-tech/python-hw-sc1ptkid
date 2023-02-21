d78ee28a46975edf68a13bbcd3217f679da51b12
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-21 22:16:52 +0100

    Update task8.py

--- src/lms127/task8.py
+++ src/lms127/task8.py
@@ -2,11 +2,9 @@ from to_do import TODO
 
 
 def task8(sentence, character):
-
     result = sentence.count(character)
     return result
 
 
 if __name__ == "__main__":
-
-    print("result is",task8(sentence="I code in KOTLIN", character="I"))
+    print("result is", task8(sentence="I code in KOTLIN", character="I"))
