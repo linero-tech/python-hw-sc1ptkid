f4e6fa10fb9181927bd0f65c19bc1db63ff2aafd
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-22 19:59:03 +0100

    Update task9.py

--- src/lms127/task9.py
+++ src/lms127/task9.py
@@ -2,9 +2,7 @@ from to_do import TODO
 
 
 def task9(sentence, character):
-    result = character.lower() in sentence.lower()
-    return result
-
+    return character.lower() in sentence.lower()
 
 if __name__ == "__main__":
     print("result is", task9(sentence="I code In KOTLIN", character="i"))
