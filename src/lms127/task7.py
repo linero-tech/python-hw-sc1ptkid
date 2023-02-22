f3e0632c310d7cdf9745f89e7352cd7a48cbb774
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-02-22 20:03:24 +0100

    Update task7.py

--- src/lms127/task7.py
+++ src/lms127/task7.py
@@ -2,9 +2,8 @@ from to_do import TODO
 
 
 def task7(sentence):
-    result = len(sentence)
-    return result
-
+    return len(sentence)       #result = len(sentence)
+    # return result
 
 if __name__ == "__main__":
     print(task7(sentence="I Love GBG"))
