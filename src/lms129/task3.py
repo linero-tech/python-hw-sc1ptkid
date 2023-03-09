81fa67e64c57d3fddde835e79003e0e6fdcfa72e
Camilo <55490613+sc1ptkid@users.noreply.github.com>
2023-03-09 15:31:02 +0100

    Update task3.py

--- src/lms129/task3.py
+++ src/lms129/task3.py
@@ -2,6 +2,15 @@ from to_do import TODO
 
 
 def task3(number):
-    return TODO(
-        "Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword"
-    )
+    result = 1
+    for i in range(number, 0, -1):
+        result = result * i
+
+    return result
+
+
+if __name__ == "__main__":
+    print(task3(number=5))  # 120
+    print(task3(number=3))  # 6
+    print(task3(number=0))  # 1
+    print(task3(number=4))  # 24
